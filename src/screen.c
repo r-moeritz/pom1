536a32e95c0180b2bb22e62a37c08d897feddeb7
Gerald DEGR00TE <br0nt05aur3@proton.me>
2025-04-03 11:34:25 +0200

    Update screen.c

--- src/screen.c
+++ src/screen.c
@@ -44,11 +44,10 @@ int loadCharMap(void)
 	{
 		fread(charac, 1, 1024, fp);
 		fclose(fp);
+		return 1;
 	}
 	else
 		return 0;
-
-	return 1;
 }
 
 void setPixelSize(int ps)
@@ -137,23 +136,22 @@ static void outputDsp(unsigned char dsp)
 	writeDsp(dsp);
 }
 
-static void drawCharac(int xPosition, int yPosition, unsigned char r, unsigned char g, unsigned char b, unsigned char characNumber)
+static void drawCharac(int xPosition, int yPosition, unsigned char characNumber)
 {
 	SDL_Rect rect;
 	int k, l;
-
+	rect.w = pixelSize;
 	for (k = 0; k < 7; k++)
 	{
+		rect.y = yPosition + pixelSize * (k + 1);
 		for (l = 1; l < 6; l++)
 		{
 			if (charac[characNumber * 8 + k] & (0x01 << l))
 			{
 				rect.x = xPosition + pixelSize * l;
-				rect.y = yPosition + pixelSize * (k + 1);
-				rect.w = pixelSize;
 				rect.h = pixelSize - (_scanlines ? 1 : 0);
 
-				SDL_FillRect(screen, &rect, SDL_MapRGB(screen->format, r, g, b));
+				SDL_FillRect(screen, &rect, 0xFFFFFF);
 			}
 		}
 	}
@@ -208,7 +206,7 @@ static void drawBlinkingCursor(void)
 		if (clearCursor)
 			SDL_FillRect(screen, &rect, 0);
 		else
-			drawCharac(rect.x, rect.y, 255, 255, 255, (unsigned char)(_blockCursor ? 0x01 : 0x40));
+			drawCharac(rect.x, rect.y, (unsigned char)(_blockCursor ? 0x01 : 0x40));
 
 		SDL_UpdateRect(screen, rect.x, rect.y, rect.w, rect.h);
 			
@@ -230,12 +228,12 @@ void redrawScreen(void)
 			xPosition = i * pixelSize * 7;
 			yPosition = j * pixelSize * 8;
 				
-			drawCharac(xPosition, yPosition, 255, 255, 255, screenTbl[j * 40 + i]);
+			drawCharac(xPosition, yPosition, screenTbl[j * 40 + i]);
 		}
 	}
 
 	if (!_blinkCursor)
-		drawCharac(indexX * pixelSize * 7, indexY * pixelSize * 8, 255, 255, 255, (unsigned char)(_blockCursor ? 0x01 : 0x40));
+		drawCharac(indexX * pixelSize * 7, indexY * pixelSize * 8, (unsigned char)(_blockCursor ? 0x01 : 0x40));
 
 	SDL_UpdateRect(screen, 0, 0, 0, 0);
 }
@@ -265,16 +263,16 @@ void updateScreen(void)
 		drawBlinkingCursor();
 }
 
-void drawCharacter(int xPosition, int yPosition, unsigned char r, unsigned char g, unsigned char b, unsigned char characNumber)
+void drawCharacter(int xPosition, int yPosition, unsigned char characNumber)
 {
 	if (_scanlines)
 	{
 		_scanlines = 0;
-		drawCharac(xPosition, yPosition, 0, 0, 0, characNumber);
+		drawCharac(xPosition, yPosition, characNumber);
 		_scanlines = 1;
 	}
 	else
-		drawCharac(xPosition, yPosition, 0, 0, 0, characNumber);
+		drawCharac(xPosition, yPosition, characNumber);
 }
 
 void initScreen(void)
